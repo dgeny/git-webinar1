7a52a415d11d7bcb5f1fc4ab8b5fbe90f13ca0df
Eugene N <kan1fol@ya.ru>
2023-11-28 09:20:33 +0200

    add custom print function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pandas as pd
 import seaborn as sns 
 
 
+def pp_dataset(ds: pd.DataFrame):
+    df = pd.DataFrame(ds['registered'].mean())
+    df.merge(ds.cnt.mean(), left_index=on, right_index=on)
+    print(df)
+
 def main():
     dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
     print(dataset.info())
