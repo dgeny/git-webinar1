676853ae822cfb3c11e5cb7b5e3ac8281a8542e2
Eugene N <kan1fol@ya.ru>
2023-11-28 09:34:00 +0200

    add custom print function #2

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ def pp_dataset(ds: pd.DataFrame):
     med = ds['registered'].median()
     print(f'Registered values: mean - {mn}, median - {med} ')
 
+
+def pp_dataset2(ds: pd.DataFrame):
+    mn = ds['cnt'].mean()
+    med = ds['cnt'].median()
+    pp_dataset(ds)
+    print(f'Count values: mean - {mn}, median - {med} ')
+
 def main():
     dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
-    pp_dataset(dataset)
+    pp_dataset2(dataset)
 
 if __name__ == '__main__':
     main()
