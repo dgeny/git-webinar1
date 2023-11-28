cb0da29be0b85adc3751b533e10ba845220b1c3f
Eugene N <kan1fol@ya.ru>
2023-11-28 09:42:08 +0200

    dataset by month resampling and printing

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def pp_dataset(ds: pd.DataFrame):
 def main():
     dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
     pp_dataset(dataset)
+    print('======== resample by month ===========')
+    dataset['dteday'] = dataset['dteday'].astype('datetime64[ns]')
+    dataset = dataset.reset_index().set_index('dteday')
+    dataset = dataset.resample('W').sum([ 'registered', 'cnt']).filter([ 'registered', 'cnt'])
+    pp_dataset(dataset)
 
 if __name__ == '__main__':
     main()
