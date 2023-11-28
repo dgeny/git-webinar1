af404008c964e325d0e1fb8ce67196e4903f26ba
Eugene N <kan1fol@ya.ru>
2023-11-28 09:21:40 +0200

    custom print dataset

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import seaborn as sns
 
 
 def pp_dataset(ds: pd.DataFrame):
-    df = pd.DataFrame(ds['registered'].mean())
-    df.merge(ds.cnt.mean(), left_index=on, right_index=on)
-    print(df)
+    mn = ds['registered'].mean()
+    med = ds['registered'].median()
+    print(f'Registered values: mean - {mn}, median - {med} ')
 
 def main():
     dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
-    print(dataset.info())
+    pp_dataset(dataset)
 
 if __name__ == '__main__':
     main()
