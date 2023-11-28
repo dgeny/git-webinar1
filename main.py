0845792311bb7d255cce5f6b4e038d3f32e5107f
Eugene N <kan1fol@ya.ru>
2023-11-28 10:53:09 +0200

    Revert "git reset --mixed by task #3" undo reset with --hard option
    This reverts commit 69926c99b18648c96dcae70683cf6ea4f8e76a9e.

--- main.py
+++ main.py
@@ -15,11 +15,26 @@ def pp_dataset(ds: pd.DataFrame):
 def main():
     dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
     pp_dataset(dataset)
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+=======
+def main():
+    dataset = pd.read_csv('https://github.com/SamHuang1018/Bike-Sharing-from-UCI-datasets/raw/main/day.csv')
+    pp_dataset(dataset)
+>>>>>>> parent of 676853a (add custom print function #2)
+=======
+>>>>>>> parent of 69926c9 (git reset --mixed by task #3)
     print('======== resample by month ===========')
     dataset['dteday'] = dataset['dteday'].astype('datetime64[ns]')
     dataset = dataset.reset_index().set_index('dteday')
     dataset = dataset.resample('W').sum([ 'registered', 'cnt']).filter([ 'registered', 'cnt'])
     pp_dataset(dataset)
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 9046d58 (remove resampling by example)
+>>>>>>> parent of 69926c9 (git reset --mixed by task #3)
 
 if __name__ == '__main__':
     main()
